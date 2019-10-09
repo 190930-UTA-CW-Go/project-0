ee78b07488e79c998e25e6bcca182f647dea0638
Tony-Moon <anthonyowenmoon@gmail.com>
2019-10-08 19:05:25 -0500

    Changes to documentation of gen packagage

--- gen/Generate.go
+++ gen/Generate.go
@@ -1,5 +1,7 @@
 package gen
 
+import "fmt"
+
 /*
 Generate is the main function inside the gen package. Really, Generate should
 be the only function in the gen package to be called outside of the gen and the
@@ -8,10 +10,13 @@ main.go inside the main package.
 */
 func Generate(rows int, columns int, max int) ([]string, []string, []int) {
 
-	Index := MakeIndex(rows, columns)
-	Beverage := MakeBeverage(rows, columns)
-	Stock := MakeStock(rows, columns, max)
-
-	return Index, Beverage, Stock
-
+	if !(rows <= 0) || (columns <= 0) || (max <= 0) {
+		Index := MakeIndex(rows, columns)
+		Beverage := MakeBeverage(rows, columns)
+		Stock := MakeStock(rows, columns, max)
+		return Index, Beverage, Stock
+	}
+	fmt.Println("Error! Could not generate machine.")
+	fmt.Println("One or more field has been left blank or made less than 1.")
+	return nil, nil, nil
 }
