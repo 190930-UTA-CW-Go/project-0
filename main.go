7bae4307bf6c02a6e58d71b20e1835fb65b151b2
KenFletcherGO <texas5starlanguageservices@gmail.com>
2019-10-08 20:22:57 -0500

    Researched package definition for "bufio, io, and map. Attempted to arrange functions and statements from the map definition related to accountID and password log-in. Found example on github.com

--- main.go
+++ main.go
@@ -1,7 +1,168 @@
 package main
 
 import "fmt"
+import "io"
 
 func main() {
-	fmt.Println("Hello, Go!")
+
+if status !== nil {
+	
+	println("How can we help you?")
+
+} else {
+
+	println("We're sorry, service is currently unavailable. Please try again later")
 }
+	return status;
+
+func login() {
+
+	if status !==nil {
+
+		var [] accountID := fmt.Println("Please enter your account ID: " + accountID)
+	}
+		fmt.Println("Please enter your password: "  + password)
+
+		 m = make(map[accountID string]password string)
+
+
+
+m [accountID] == string []
+
+
+
+
+
+//This statement retrieves the value stored under the key "route" and assigns it to a new variable i:
+
+
+if map[accountID string]password string {
+
+	println ("You have successfully logged in." \n "Welcome.")
+
+
+} else {
+
+	println("Please try again.")
+}
+	
+}
+
+i := m["accountID"]
+
+println("Hello, accountID   " + i)
+
+
+//If the requested key doesn't exist, we get the value type's zero value. In this case the value type is int, so the zero value is 0:
+
+j := m["root"]
+//if status !==nil {
+
+	var [] accountID := fmt.Println("Please enter your account ID: " + accountID)
+}
+	fmt.Println("Please enter your password: "  + password)
+
+	 m = make(map[accountID string]password string)
+
+
+
+m [accountID] == string []
+
+
+//This statement retrieves the value stored under the key "route" and assigns it to a new variable i:
+
+
+if map[accountID string]password string {
+
+println ("You have successfully logged in." \n "Welcome.")
+
+
+} else {
+
+println("Please try again.")
+}
+
+}
+
+i := m["accountID"]
+
+println("Hello, accountID   " + i)
+
+/*
+If the requested key doesn't exist, we get the value type's zero value. In this case the value type is int, so the zero value is 0:
+
+j := m["root"]
+j == 0
+
+The built in len function returns on the number of items in a map:
+
+n := len(m)
+
+The built in delete function removes an entry from the map:
+
+delete(m, "route")
+
+The delete function doesn't return anything, and will do nothing if the specified key doesn't exist.
+
+A two-value assignment tests for the existence of a key: 
+
+
+ j == 0
+
+The built in len function returns on the number of items in a map:
+
+n := len(m)
+
+The built in delete function removes an entry from the map:
+
+delete(m, "route")
+
+The delete function doesn't return anything, and will do nothing if the specified key doesn't exist.
+
+A two-value assignment tests for the existence of a key: */
+	
+
+/*REFERENCE: 
+
+package main
+
+import (
+	"database/sql"
+	"net/http"
+	"log"
+	_ "github.com/lib/pq"
+)
+
+const hashCost = 8
+var db *sql.DB
+
+func main() {
+	// "Signin" and "Signup" are handler that we will implement
+	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/signup", Signup)
+	// initialize our database connection
+	initDB()
+	// start the server on port 8000
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func initDB(){
+	var err error
+	// Connect to the postgres db
+	//you might have to change the connection string to add your database credentials
+	db, err = sql.Open("postgres", "dbname=mydb sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
+
+
+
+	
+
+
+}
+	
+
+}
+*/
