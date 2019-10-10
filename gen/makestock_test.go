44e310510ef544bc261fbfc9ab12d874570fbc03
Tony-Moon <anthonyowenmoon@gmail.com>
2019-10-10 16:54:09 -0500

    MakeStock unit test

--- gen/makestock_test.go
+++ gen/makestock_test.go
@@ -5,5 +5,12 @@ import (
 )
 
 func TestMakeStock(t *testing.T) {
-
+	x := MakeStock(2, 2, 2)
+	for i := range x {
+		if (x[i] > 2) || (x[i] < 0) {
+			t.Errorf("Stock out of range")
+		} else {
+			t.Log("Stock works normally")
+		}
+	}
 }
