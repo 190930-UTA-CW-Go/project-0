3b004ca79ee92719d84e73e3495782462e74805b
Tony-Moon <anthonyowenmoon@gmail.com>
2019-10-10 11:54:42 -0500

    Got database working

--- db/init.sql
+++ db/init.sql
@@ -11,3 +11,39 @@ create table drinklist (
     brand varchar,
     prob float
 );
+
+insert into drinklist values (01, 'Duda-Cola',    'Duda-Cola', 5);
+insert into drinklist values (02, 'Sprunk',       'Duda-Cola', 5);
+insert into drinklist values (03, 'Diet Dude',    'Duda-Cola', 4);
+insert into drinklist values (04, 'Dunce',        'Duda-Cola', 4);
+insert into drinklist values (05, 'BBQ''s',       'Duda-Cola', 4);
+insert into drinklist values (06, 'Fat-ah',       'Duda-Cola', 3);
+insert into drinklist values (07, 'Electromaid',  'Duda-Cola', 3);
+insert into drinklist values (08, 'Gold Snow',    'Duda-Cola', 3);
+insert into drinklist values (09, 'Breaker',      'Duda-Cola', 2);
+insert into drinklist values (10, 'Metal Pedal',  'Duda-Cola', 2);
+insert into drinklist values (11, 'Ab Energy',    'Duda-Cola', 1);
+
+insert into drinklist values (12, 'Salt-PhD',     'Salt-PhD', 5);
+insert into drinklist values (13, 'Mexico-Wet',   'Salt-PhD', 5);
+insert into drinklist values (14, 'Ph-1',         'Salt-PhD', 4);
+insert into drinklist values (15, 'DDijon-Brown', 'Salt-PhD', 4);
+insert into drinklist values (16, 'Q&A',          'Salt-PhD', 4);
+insert into drinklist values (17, 'Squish',       'Salt-PhD', 3);
+insert into drinklist values (18, 'SomeWatch',    'Salt-PhD', 3);
+insert into drinklist values (19, 'Oh-Snap',      'Salt-PhD', 3);
+insert into drinklist values (20, 'Fiji-Slap',    'Salt-PhD', 2);
+insert into drinklist values (21, 'Poison',       'Salt-PhD', 2);
+insert into drinklist values (22, 'Boohoo',       'Salt-PhD', 1);
+
+insert into drinklist values (23, 'Tipsy',        'TipsyCo', 5);
+insert into drinklist values (24, 'LowPool',      'TipsyCo', 5);
+insert into drinklist values (25, 'ZeroTipsy',    'TipsyCo', 4);
+insert into drinklist values (26, 'Blue-Gas',     'TipsyCo', 4);
+insert into drinklist values (27, 'Mutt',         'TipsyCo', 4);
+insert into drinklist values (28, 'Hot-Cabana',   'TipsyCo', 3);
+insert into drinklist values (29, 'Geckoaide',    'TipsyCo', 3);
+insert into drinklist values (30, 'Lipbalm',      'TipsyCo', 3);
+insert into drinklist values (31, 'Warm',         'TipsyCo', 2);
+insert into drinklist values (32, 'Hippie',       'TipsyCo', 2);
+insert into drinklist values (33, 'Nude',         'TipsyCo', 1);
