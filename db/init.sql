1d67312b14738099759d23c83339583bbd595828
David Chang <davidychang@outlook.com>
2019-10-11 10:07:40 -0500

    updated init.sql with account table

--- db/init.sql
+++ db/init.sql
@@ -11,3 +11,12 @@ create table employee (
     first_name varchar,
     last_name varchar
 );
+
+create table account (
+    email varchar references customer(email),
+    acc_type varchar,
+    acc_num varchar,
+    acc_balance float
+);
+
+insert into employee values ('user', 'pass', 'David', 'Chang');
