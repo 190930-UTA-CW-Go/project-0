d671f9d9f3fedca5e23897eb76b77f96c124acbb
ggarnerdeng <ggarnerdeng@gmail.com>
2019-10-14 11:14:34 -0500

    "sighs"

--- db/init.sql
+++ db/init.sql
@@ -2,7 +2,7 @@ create table customerLogin (
     userName varchar primary key,
     password varchar NOT NULL,
     fname varchar NOT NULL,
-    lname varchar NOT NULL,
+    lname varchar NOT NULL
 );
 insert into customerLogin values ('Bulbasaury', 'pAsSwOrdd', 'Bulb', 'saur');
 insert into customerLogin values ('adfh', 'password', 'Kdfa', 'miakhalifa');
@@ -12,13 +12,13 @@ create table customerAccounts(
     routingNumber serial primary key,
     balance float,
     userName varchar,
-    userName2 varchar,
-)
+    userName2 varchar
+);
 insert into customerAccounts values (123123, 500, 'Bulbasaury', 'adfh' );
 
 
 create table employeeAccounts(
     adminLogin varchar primary key,
-    password varchar NOT NULL,
-)
+    password varchar NOT NULL
+);
 insert into employeeAccounts values ('god', 'password');
--- main.go
+++ main.go
@@ -86,3 +86,19 @@ func ping(db *sql.DB) {
 	row.Scan(&userName, &password, &name, &balance)
 	fmt.Println(userName, password, name, balance)
 }*/
+
+/*
+create table customerAccounts(
+    routingNumber serial primary key,
+    balance float,
+    userName varchar,
+    userName2 varchar
+)
+insert into customerAccounts values (123123, 500, 'Bulbasaury', 'adfh' );
+
+
+create table employeeAccounts(
+    adminLogin varchar primary key,
+    password varchar NOT NULL
+)
+insert into employeeAccounts values ('god', 'password');*/
