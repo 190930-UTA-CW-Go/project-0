3357864a321cecf72be5d35c16b56a5310c538fd
Tony-Moon <anthonyowenmoon@gmail.com>
2019-10-16 10:38:17 -0500

    bug fix

--- vend/GetDrinks.go
+++ vend/GetDrinks.go
@@ -6,17 +6,28 @@ import "database/sql"
 GetDrinks documentation
 */
 func GetDrinks(db *sql.DB) ([]string, []string, []int, string) {
-	rows, _ := db.Query("SELECT * FROM machine;")
-	i := 0
-	index := make([]string, 2)
-	name := make([]string, 2)
-	stock := make([]int, 2)
-	var brand string
+	var index, name, brand string
+	var stock, i, count int
 
+	rows0, _ := db.Query("SELECT COUNT(*) as count FROM machine;")
+	for rows0.Next() {
+		rows0.Scan(&count)
+	}
+
+	inDB := make([]string, count)
+	naDB := make([]string, count)
+	stDB := make([]int, count)
+
+	rows, _ := db.Query("SELECT * FROM machine;")
+	i = 0
 	for rows.Next() {
-		rows.Scan(&index[i], &name[i], &stock[i], &brand)
+		rows.Scan(&index, &name, &stock, &brand)
+
+		inDB[i] = index
+		naDB[i] = name
+		stDB[i] = stock
 		i++
 	}
 
-	return index, name, stock, brand
+	return inDB, naDB, stDB, brand
 }
