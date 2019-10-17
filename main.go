49f7a4eeaa8233cf134c7fa3b8eda6c5b6f21d76
terrellg <terrellgreen56@gmail.com>
2019-10-16 21:46:30 -0500

    Add database

--- main.go
+++ main.go
@@ -1,92 +1,133 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
-	"io/ioutil"
-	"os"
+
+	_ "github.com/lib/pq"
 )
 
-//pending.json is used for all pending accounts waiting approval
-//users.json stores all approved
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "postgres"
+)
 
-/*
-Bank struct is a slice of Users to hold everyone
-*/
-type Bank struct {
-	Bank []User `json:"users"`
-}
-
-/*
-	User is a struct to represent the user
-	Holds the username and password
-
-*/
-type User struct {
-	Username string  `json:"username"`
-	Password string  `json:"password`
-	Account  Account `json:account`
-	// Approved bool      `json:approved`
-}
-
-type Account struct {
-	Balance float32 `json:balance`
-}
+var db *sql.DB
 
 func main() {
-
-	file, err := os.Open("users.json")
-	defer file.Close()
+	var err error
+	datasource := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err = sql.Open("postgres", datasource)
+	defer db.Close()
 	if err != nil {
-		fmt.Println("Could not find users.json")
+		panic(err)
+	}
+	mainmenu()
+	//signup()
+	// login()
+
+}
+
+func signup() {
+	var username, fname, lname, password string
+	fmt.Println("Sign up for an account")
+	fmt.Println("enter your first name")
+	fmt.Scanln(&fname)
+	fmt.Println("enter your last name")
+	fmt.Scanln(&lname)
+	fmt.Println("enter a username")
+	fmt.Scan(&username)
+
+	fmt.Println("enter a password")
+	fmt.Scan(&password)
+
+	sql := `
+	insert into employees (fname, lname, username, pass, reimbursement, currentstatus)
+	values ($1, $2, $3, $4, $5, $6)`
+
+	_, err := db.Exec(sql, fname, lname, username, password, "he", "dadkls")
+	if err != nil {
+		fmt.Println("usrname already exists")
+	} else {
+
+		fmt.Println("Registration successful")
 
 	}
-	fmt.Println("Successfully Opened users.json")
+}
 
-	byteValue, _ := ioutil.ReadAll(file)
+func login() {
+	var username string
+	var password string
+	var placeholder string
+	fmt.Println("Login to your account")
+	fmt.Println("enter your username")
+	fmt.Scan(&username)
+	fmt.Println("enter your password")
+	fmt.Scan(&password)
 
-	var users Bank
+	sql := `select pass from employees where username = $1`
 
-	json.Unmarshal(byteValue, &users)
+	result := db.QueryRow(sql, username)
 
-	//print everything in json
-	fmt.Println(users)
+	result.Scan(&placeholder)
+
+	if placeholder != password {
+		fmt.Println("Invalid username/password")
+		fmt.Println()
+		login()
+	} else {
+		fmt.Println("Login Successful")
+
+		fmt.Println("Employee Portal")
+	}
 
 }
 
+func mainmenu() {
+	var selection string
+	fmt.Println("Welcome to the reimbursemnt app")
+	fmt.Println("Enter 1 to sign up for an account")
+	fmt.Println("Enter 2 login")
+	fmt.Scan(&selection)
+
+	switch selection {
+	case "1":
+		signup()
+	case "2":
+		login()
+
+	default:
+		fmt.Println("Invalid Option")
+		mainmenu()
+
+	}
+}
+
 /*
-Signup adds a username and password to pending json file
+
+cd db
+sudo docker build -t project .
+sudo docker run --name mydb -d -p 5432:5432 project
+
+
+reset dabatase
+sudo docker stop mydb
+sudo docker rm mydb
+sudo docker rmi project
+
+
+check database
+sudo docker ps-a
+sudo docker images -a
+
+
+
+to start
+go run *.go
+go run *.go  -hide
 
 */
-func Signup() {
-
-}
-
-/*
-
-
- */
-func Login() {
-
-}
-
-/*
-
- */
-func Viewbalance() {
-
-}
-
-/*
-
- */
-func Withdraw() {
-
-}
-
-/*
-
- */
-func Deposit() {
-
-}
